86db0a217dcfc89cdbce9b2021aed68458cf2264
IkoAfianando <ikoafianando123@gmail.com>
2021-12-10 08:45:14 +0700

    Initial commit

---
+++ src/main/java/programmingwithiko/belajarspringdasar/BelajarSpringDasarApplication.java
@@ -0,0 +1,15 @@
+package programmingwithiko.belajarspringdasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringDasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringDasarApplication.class, args);
+	}
+		static {
+			System.out.println("Iko Afianando adalah seorang Programmer yang Handal dalam program Java Development");
+		}
+}
---
+++ src/test/java/programmingwithiko/belajarspringdasar/BelajarSpringDasarApplicationTests.java
@@ -0,0 +1,13 @@
+package programmingwithiko.belajarspringdasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringDasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
